2fb5f432c11bcd51f26d2260502d2bfbaeebb703
JALZ <jannus.largo@correounivalle.edu.co>
2023-03-16 22:52:28 -0500

    solve path files js and add slides carrousel home

--- AmazingEventsPresentation/js/home.js
+++ AmazingEventsPresentation/js/home.js
@@ -1,13 +1,34 @@
 function addHomeEvents() {
     for (let i = 0; i < eventos.eventos.length; i++) {
-        let CardImage = eventos.eventos[i].image;
-        let CardName = eventos.eventos[i].name;
-        let CardDescription = eventos.eventos[i].description;
-        let CardPrice = eventos.eventos[i].price;
-        const homeEvents = createCardDiv(CardImage, CardName, CardDescription, CardPrice);
+        const slide = document.createElement('div');
+        slide.classList.add("swiper-slide", "d-flex");
+        for (let j = 0; j < 4; j++) {
+            let CardImage = eventos.eventos[i].image;
+            let CardName = eventos.eventos[i].name;
+            let CardDescription = eventos.eventos[i].description;
+            let CardPrice = eventos.eventos[i].price;
+            const homeEvents = createCardDiv(CardImage, CardName, CardDescription, CardPrice);
+            slide.appendChild(homeEvents);
+            i++;
+        }
         const container = document.querySelector('.container');
-        container.appendChild(homeEvents);
-
+        container.appendChild(slide);
     }
 }
 addHomeEvents();
+
+
+let swiper = new Swiper("swiper-container", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+},
+);
